bc055a5b4b164454a2cd4d454ac6f8858abf12ca
john ackers <john ackers@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-28 14:38:15 +0000

    change settings page to view default and current layouts, prefix all options with ec_
    git-svn-id: https://plugins.svn.wordpress.org/ecampaign/trunk@365833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ecampaign.php
+++ ecampaign.php
@@ -3,7 +3,7 @@
  Plugin Name: Ecampaign
  Plugin URI: http://wordpress.org/extend/plugins/ecampaign/
  Description: Allows a simple email based campaign action to be embedded into any wordpress page or post.
- Version: 0.73
+ Version: 0.74
  Author: John Ackers
  Author URI: john.ackers ymail.com
 
@@ -73,17 +73,15 @@ add_shortcode('ecampaign', 'ecampaign_short_code');
 
 function ecampaign_unset_options()
 {
-  delete_option('campaignEmail' );
-  delete_option('layout' );
-  delete_option('checkbox1' );
-  delete_option('checkbox2' );
-  delete_option('thankYouText' );
-  delete_option('inviteSubscriptionText' );
-  delete_option('inviteFriendsText' );
-  delete_option('testMode' );
-  delete_option('mailer' );
-  delete_option('checkdnsrr' );
-
+  delete_option('ec_campaignEmail' );
+  delete_option('ec_layout' );
+  delete_option('ec_checkbox1' );
+  delete_option('ec_checkbox2' );
+  delete_option('ec_thankYouText' );
+  delete_option('ec_friendsLayout' );
+  delete_option('ec_testMode' );
+  delete_option('ec_mailer' );
+  delete_option('ec_checkdnsrr' );
 }
 register_uninstall_hook(__FILE__, 'ecampaign_unset_options');
 
@@ -116,127 +114,197 @@ function ecampaign_menu() {
 }
 
 
+/**
+ * Bind these options to settings page which will make
+ * wordpress save them.
+ *
+ * @return unknown_type
+ */
 
-// not sure how to make the methods below load dynamically
+
+function ecampaign_registerOptions()
+{
+//  ecampaign_unset_options();
+  register_setting( 'ec-settings', 'ec_testMode' );
+  register_setting( 'ec-settings', 'ec_campaignEmail' );
+  register_setting( 'ec-settings', 'ec_layout' );
+  register_setting( 'ec-settings', 'ec_checkbox1' );
+  register_setting( 'ec-settings', 'ec_checkbox2' );
+  register_setting( 'ec-settings', 'ec_thankYouText' );
+  register_setting( 'ec-settings', 'ec_friendsLayout' );
+  register_setting( 'ec-settings', 'ec_testMode' );
+  register_setting( 'ec-settings', 'ec_mailer' );
+  register_setting( 'ec-settings', 'ec_checkdnsrr' );
+}
 
 
-function ecampaign_registerOptions() {
-  //register our settings
-  register_setting( 'ecampaign-settings-group', 'campaignEmail' );
-  register_setting( 'ecampaign-settings-group', 'layout' );
-  register_setting( 'ecampaign-settings-group', 'checkbox1' );
-  register_setting( 'ecampaign-settings-group', 'checkbox2' );
-  register_setting( 'ecampaign-settings-group', 'thankYouText' );
-  register_setting( 'ecampaign-settings-group', 'inviteSubscriptionText' );
-  register_setting( 'ecampaign-settings-group', 'inviteFriendsText' );
-  register_setting( 'ecampaign-settings-group', 'testMode' );
-  register_setting( 'ecampaign-settings-group', 'mailer' );
-  register_setting( 'ecampaign-settings-group', 'checkdnsrr' );
+/**
+ * add options but put prefix in front of all names.
+ * To provide forward, compatability with old versions,
+ * use values of old, unprefixed names if they exist
+ */
+
+function ecampaign_add_option($name, $prompt, $defaultValue, &$keys, $autoload)
+{
+  $val = get_option($name);
+  if (empty($val))
+  {
+    $oldName = substr($name,3); // strip off ec_
+    $val = get_option($oldName);
+    if (!empty($val))
+    {
+      add_option($name, $val,'', 'no');
+      delete_option($oldName);
+      return ;
+    }
+  }
+  add_option($name, $defaultValue,'', $autoload);
+  $keys[0][$name] = $prompt;		// save prompts
+  $keys[1][$name] = $defaultValue;	// and default values
 }
 
 
 
-function ecampaign_options() {
+function ecampaign_options()
+{
+  $prompt = array(); $default = array();
+  $keys =  array(&$prompt, &$default);
+
+  ecampaign_add_option('ec_testMode',
+  __("Test Mode: Changes the <strong>To:</strong> field of the outgoing email from 'targetEmail' to 'campaignEmail'. ") .
+  __("This feature prevents emails being sent to the target organisation accidentally."), '', $keys, 'no');
 
   // copies of emails are sent here
-  add_option('campaignEmail', '', null, 'no');
+
+  ecampaign_add_option('ec_campaignEmail',
+  __("Campaign mailbox address 'campaignEmail'; copies of activists emails are sent here. You can override it on
+      individual email alerts."), 'campaign.email@not.set.up', $keys, 'no');
 
   // setting up the default form layout
-  add_option('layout', '
-  %to
+
+  ecampaign_add_option('ec_layout',
+
+  __("<p>Form template. Add and remove fields that you want to collect. Valid fields are: ") .
+     "%to %subject %body %name %address1 %address2 %address3 %city %ukpostcode %postcode %zipcode %state %country " .
+     "%email %send %checkbox1 %checkbox2. </p><p>" .
+  __("Adjust size of body field using %44.55body where 44 represents number of columns and 55 represents number of rows. ").
+  __("Adjust size of other fields using %33.10city where 33 represents field size and 10 is minimum number of characters required.").
+    "<p/><p>".
+  __("You may have to tweak the style sheet if you make significant changes.</p>"),
+  '%to
   %subject
-  %body'
-  . __("Your name and address as entered below will be added. Do not sign your name above.")
-  . __("All fields are needed.") .
- '%name
+  %body
+     <div id="text-guidance">'
+  . __("Your name and address as entered below will be added. Do not sign your name above. ")
+  . __("All fields are needed.")
+  .  '</div>
+  %name
   %zipcode
   %email
   %checkbox1
   %checkbox2
   %send
-  <div class="clear advisory">'
-  . __("Please contact %campaignEmail if you have any difficulties or queries")
-  . '</div>', null, true);
+     <div id = "text-contact">'. __("Please contact %campaignEmail if you have any difficulties or queries"). '</div>',
+  $keys, 'no');
 
-  add_option('checkbox1', __('Check if you want to receive updates about this campaign.'), null, 'no');
+  ecampaign_add_option('ec_checkbox1',
+    __("Prompt for %checkbox1 which can be included in the form above."),
+    __('Check if you want to receive updates about this campaign.'), $keys, 'no');
 
-  add_option('checkbox2', __('Check if you want to receive alerts about related campaigns.'), null, 'no');
+  ecampaign_add_option('ec_checkbox2',
+    __("Prompt for %checkbox2 which can be included in the form above."),
+    __('Check if you want to receive alerts about related campaigns.'), $keys, 'no');
 
-  add_option('thankYouText',
-  '<p style="font-weight:bold;">'
+  ecampaign_add_option('ec_thankYouText',
+   __("Text sent after successfully sending the email. The class and id attrubutes included
+       in default values on this page are only used by the default style sheet ecampaign.css."),
+
+  '<div id="text-sent-1" class="ecOk">'
   . __('Your email has been sent.')
-  . '</p><p>'
-  . _('You should receive a copy in your mailbox. Thank you for taking part in this alert.</p>'), null, 'no');
+  . '</div><div id="text-sent-2" class="ecOk">'
+  . _('You should receive a copy in your mailbox. Thank you for taking part in this alert.</div>'), $keys, 'no');
 
-  add_option('inviteFriendsText',
-  '<h2>' . __('Tell your friends') . '</h2><p>' .
-  __('You can also send an email to friends asking them to take part in the email alert.
-  See below for the text that will be sent.') . '</p>', null, 'no');
+  ecampaign_add_option('ec_friendsLayout',
+  "<p>".
+   __("Friends form template. Add and remove fields that you want to collect. Valid fields are:").
+      "%friendSubject %friendBody %friendEmail %friendSend." .
+   __("Adjust field length the same as for Form layout above. ").
+   __("You have to include the %send button somewhere in the form.</p>"),
+  '<h3 id="text-friends">' . __('What about telling your friends?') . '</h3>
+   %friendSubject
+   %friendBody
+   %friendEmail
+   %friendSend',$keys, 'no');
 
   // transport used by PHPMailer to send mail
-  add_option('mailer', 'mail', null, 'no');
+  ecampaign_add_option('ec_mailer',
+  __("'Mailer' transport used by PHPmailer (mail, sendmail, smtp)"), 'mail', $keys, 'no');
 
-  add_option('checkdnsrr', '', null, 'no');
-
+  ecampaign_add_option('ec_checkdnsrr',
+  __("Enable checking DNS records for the domain name when checking for a valid E-mail address. "), 'yes', $keys, 'no');
 
 ?>
 <div class="wrap">
 <h2>Ecampaign settings</h2>
 
 <form method="post" action="options.php">
-    <?php settings_fields( 'ecampaign-settings-group' ); ?> <!--  Output nonce, action, and option_page fields -->
-    <table class="form-table">
+    <?php settings_fields( 'ec-settings' ); ?> <!--  Output nonce, action, and option_page fields -->
+    <table class="form-table" id="ec-settings">
 
         <tr valign="top">
-        <th scope="row"><?php echo(__("Test Mode: Changes the <strong>To:</strong> field of the outgoing email
-        from 'targetEmail' to 'campaignEmail'") .
-        __("This feature prevents emails being sent to the target organisation accidentally.")) ?> </th>
-        <td><input type="checkbox" name="testMode" <?php if (strlen(get_option('testMode')) > 0 ) echo " checked=true ";  ?> /></td>
+        <th scope="row"><?php echo $prompt["ec_testMode"] ?> </th>
+        <td><input type="checkbox" name="ec_testMode" value='1' <?php  checked(get_option('ec_testMode'), 1); ?> /></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("Campaign mailbox address 'campaignEmail'; copies of activists emails are sent here.
-        You can override it on individual email alerts.")  ?> </th>
-        <td><input type="text" name="campaignEmail" size=50 value="<?php echo get_option('campaignEmail'); ?>" /></td>
+        <th scope="row"><?php echo $prompt["ec_campaignEmail"] ?> </th>
+        <td><input type="text" name="ec_campaignEmail" size=35 value="<?php echo get_option('ec_campaignEmail'); ?>" /></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php echo(__("Form layout. Add and remove fields that you want to collect. Valid fields are: ") .
-        "%to %subject %body %name %address1 %address2 %city %postcode %zipcode %state %country " .
-        "%email %send %checkbox1 %checkbox2. ") .
-        __("You have to include the %send button somewhere in the form.") ; ?> </th>
-        <td><textarea name="layout" rows='10' cols='30'><?php echo get_option('layout'); ?></textarea></td>
+        <th scope="row" colspan="2"><?php echo $prompt["ec_layout"] ?></th>
+        </tr>
+        <tr valign="top">
+        <td>default:<br/><textarea name="ec_91" rows='16' cols='35' readonly='yes'><?php echo  $default['ec_layout']; ?></textarea></td>
+        <td>current:<br/><textarea name="ec_layout" rows='16' cols='35'><?php echo get_option('ec_layout'); ?></textarea></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("Text for %checkbox1 which can be included in the form above.") ?> </th>
-        <td><textarea name="checkbox1" rows='4' cols='50'><?php echo get_option('checkbox1'); ?></textarea></td>
+        <th scope="row"><?php echo $prompt["ec_checkbox1"] ?> </th>
+        <td><textarea name="ec_checkbox1" rows='4' cols='35'><?php echo get_option('ec_checkbox1'); ?></textarea></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("Text for %checkbox2 which can be included in the form above.")  ?> </th>
-        <td><textarea name="checkbox2" rows='4' cols='50'><?php echo get_option('checkbox2'); ?></textarea></td>
+        <th scope="row"><?php echo $prompt["ec_checkbox1"]  ?> </th>
+        <td><textarea name="ec_checkbox2" rows='4' cols='35'><?php echo get_option('ec_checkbox2'); ?></textarea></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("Text sent after successfully sending the email") ?> </th>
-        <td><textarea name="thankYouText" rows='4' cols='50'><?php echo get_option('thankYouText');?></textarea></td>
+        <th scope="row" colspan="2"><?php echo $prompt["ec_thankYouText"] ?></th>
+        </tr>
+        <tr valign="top">
+        <td>default:<br/><textarea name="ec_92" rows='6' cols='35' readonly='yes'><?php echo  $default['ec_thankYouText']; ?></textarea></td>
+        <td>current:<br/><textarea name="ec_thankYouText" rows='6' cols='35'><?php echo get_option('ec_thankYouText');?></textarea></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("Text to prompt the visitor to invite friends") ?> </th>
-        <td><textarea name="inviteFriendsText" rows='4' cols='50'><?php echo get_option('inviteFriendsText'); ?></textarea></td>
+        <th scope="row" colspan="2"><?php echo $prompt["ec_friendsLayout"]  ?> </th>
+        </tr>
+        <tr valign="top">
+
+        <td>default:<br/><textarea name="ec_93" rows='8' cols='35' readonly='yes'><?php echo $default['ec_friendsLayout']; ?></textarea></td>
+        <td>current:<br/><textarea name="ec_friendsLayout" rows='8' cols='35'><?php echo get_option('ec_friendsLayout'); ?></textarea></td>
         </tr>
 
         <tr valign="top">
-        <th scope="row"><?php _e("'Mailer' transport used by PHPmailer (mail, sendmail, smtp)") ?> </th>
-        <td><input type="text" name="mailer" size=10 value="<?php echo get_option('mailer'); ?>" /></td>
+        <th scope="row"><?php echo $prompt["ec_mailer"] ?></th>
+        <td><input type="text" name="ec_mailer" size=10 value="<?php echo get_option('ec_mailer'); ?>" /></td>
         </tr>
 
 
         <tr valign="top">
-        <th scope="row"><?php _e("Enable checking DNS records for the domain name when checking for a valid E-mail address. ")  ?> </th>
-        <td><input type="checkbox" name="checkdnsrr" <?php if (strlen(get_option('checkdnsrr')) > 0 ) echo " checked=true ";  ?>" /></td>
+        <th scope="row"><?php echo $prompt["ec_checkdnsrr"] ?> </th>
+        <td><input type="checkbox" name="ec_checkdnsrr" value='1' <?php checked(get_option('ec_checkdnsrr'), 1); ?> /></td>
         </tr>
 
     </table>
